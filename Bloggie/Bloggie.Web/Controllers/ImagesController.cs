4c2f12a76e4e6220315978aea09737d77e7a5855
AceAgas1151 <acmagas19@gmail.com>
2025-04-19 17:38:48 +0800

    Initial Commit for BlogPost Web App

---
+++ Bloggie/Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            // call a repository
+            var iamgeURL = await imageRepository.UploadAsync(file);
+
+            if (iamgeURL == null)
+            {
+                return Problem("Something went wrong!", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = iamgeURL });
+        }
+    }
+}
---
+++ Bloggie/Bloggie.Web/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bloggie.Web.Models.Domain;
+using System.Runtime.CompilerServices;
+
+namespace Bloggie.Web.Repositories
+{
+    public interface IBlogPostRepository
+    {
+        Task<IEnumerable<BlogPost>> GetAllAsync();
+        Task<BlogPost?> GetAsync(Guid id);
+        Task<BlogPost> AddAsync(BlogPost blogPost);
+        Task<BlogPost?> UpdateAsync(BlogPost blogPost);
+        Task<BlogPost?> DeleteAsync(Guid id);   
+    }
+}
