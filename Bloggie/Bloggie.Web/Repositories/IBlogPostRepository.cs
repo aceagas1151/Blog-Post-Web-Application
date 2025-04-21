d4af2d623482c86dd5bab2cc091831fcbf1c7b2c
AceAgas1151 <acmagas19@gmail.com>
2025-04-22 00:33:59 +0800

    -Added BlogController -Added Index chtml -Added image fix in css root -Added GetUrlHandle in BlogPost Repository and Interface.

--- Bloggie/Bloggie.Web/Repositories/IBlogPostRepository.cs
+++ Bloggie/Bloggie.Web/Repositories/IBlogPostRepository.cs
@@ -6,6 +6,7 @@ namespace Bloggie.Web.Repositories
     public interface IBlogPostRepository
     {
         Task<IEnumerable<BlogPost>> GetAllAsync();
+        Task<BlogPost?> GetByUrlHandleAsync(string urlHandle);
         Task<BlogPost?> GetAsync(Guid id);
         Task<BlogPost> AddAsync(BlogPost blogPost);
         Task<BlogPost?> UpdateAsync(BlogPost blogPost);
