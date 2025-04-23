403b2abe09a571e955a04592f1e6fe37b525e47e
AceAgas1151 <acmagas19@gmail.com>
2025-04-23 14:30:15 +0800

    Added Title to tabs Added HomeViewModel class reconfig Home Controller to accommodate Tags and Blogposts.

--- Bloggie/Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie/Bloggie.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bloggie.Web.Models;
 using Bloggie.Web.Repositories;
+using Bloggie.Web.Models.ViewModels;
 
 namespace Bloggie.Web.Controllers;
 
@@ -9,17 +10,31 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IBlogPostRepository blogPostRepository;
+    private readonly ITagRepository tagRepository;
 
-    public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+    public HomeController(ILogger<HomeController> logger, 
+        IBlogPostRepository blogPostRepository,
+        ITagRepository tagRepository)
     {
         _logger = logger;
         this.blogPostRepository = blogPostRepository;
+        this.tagRepository = tagRepository;
     }
 
     public async Task<IActionResult> Index()
     {
-        var blogPosts = await blogPostRepository.GetAllAsync();
-        return View(blogPosts);
+        //getting all blogs
+        var blogPosts = await blogPostRepository.GetAllAsync();  
+        //get all tags
+        var tags = await tagRepository.GetAllAsync();
+
+        var model = new HomeViewModel
+        {
+            BlogPosts = blogPosts,
+            Tags = tags
+        };
+
+        return View(model);
     }
 
     public IActionResult Privacy()
