5b46d0412fa7ac8d04280c3f2d51efe42004f1ce
AceAgas1151 <acmagas19@gmail.com>
2025-04-27 15:14:24 +0800

    -Add comment functionality -Display Comments -Add Admin User Controller

--- Bloggie/Bloggie.Web/Repositories/BlogPostLikeRepository.cs
+++ Bloggie/Bloggie.Web/Repositories/BlogPostLikeRepository.cs
@@ -1,5 +1,6 @@
 ﻿
 using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Repositories
@@ -12,6 +13,19 @@ namespace Bloggie.Web.Repositories
         {
             this.bloggieDbContext = bloggieDbContext;
         }
+
+        public async Task<BlogPostLike> AddLikeForBlog(BlogPostLike blogPostLike)
+        {
+            await bloggieDbContext.BlogPostsLike.AddAsync(blogPostLike);
+            await bloggieDbContext.SaveChangesAsync();
+            return blogPostLike;
+        }
+
+        public async Task<IEnumerable<BlogPostLike>> GetLikesForBlog(Guid blogPostId)
+        {
+            return await bloggieDbContext.BlogPostsLike.Where(x => x.BlogPostId == blogPostId).ToListAsync();
+        }
+
         public async Task<int> GetTotalLikes(Guid blogPostId)
         {
             return await bloggieDbContext.BlogPostsLike
--- Bloggie/Bloggie.Web/Repositories/IBlogPostLikeRepository.cs
+++ Bloggie/Bloggie.Web/Repositories/IBlogPostLikeRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace Bloggie.Web.Repositories
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
 {
     public interface IBlogPostLikeRepository
     {
         Task<int> GetTotalLikes(Guid id);
+
+        Task<IEnumerable<BlogPostLike>> GetLikesForBlog(Guid blogPostId);
+
+        Task<BlogPostLike>AddLikeForBlog(BlogPostLike blogPostLike);
     }
 }
